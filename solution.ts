6c8fba74d024a13b7c58e18fc8efa2f71e9938f1
shariyerShazan <shariyershazan1@gmail.com>
2025-11-17 12:21:43 +0600

    first commit

---
+++ solution.ts
@@ -0,0 +1,11 @@
+type Value = string | number | boolean
+
+const formatValue = (value : Value)=> {
+   if(typeof value === "string"){
+        return value.toUpperCase()
+   } else if ( typeof value === "number"){
+       return value * 10
+   } else if (typeof value === "boolean") {
+    return !value
+   }
+}
